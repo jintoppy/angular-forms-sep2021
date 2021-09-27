37238f25627367bf4201569512268fd3e8636cde
Jinto Jose <jintoppy@gmail.com>
2021-09-27 09:22:18 +0530

    forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './components/template-form/template-form.component';
 import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { ReactiveFormComponent } from './components/reactive-form/reactive-form.
     ReactiveFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
